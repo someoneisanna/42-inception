aee85687ca754e95a3967ea87f6583bd0c6829ce
someoneisanna <annacarolinataboada@gmail.com>
2024-03-26 14:22:37 +0000

    less of a mess, mariadb and wp seem to be working

--- srcs/mariadb/files/setup.sh
+++ srcs/mariadb/files/setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-set -e
-
-DB_NAME="database_example"
-DB_USERNAME="random"
-DB_PASSWORD="random"
-DB_TABLE="table_example"
-
 service mariadb start
 
+echo "Setting up MariaDB..."
+
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "USE $DB_NAME; CREATE TABLE IF NOT EXISTS $DB_TABLE (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL);"
-mysql -u root -e "USE $DB_NAME; INSERT INTO $DB_TABLE (name) VALUES ('Hello, World!');"
+
+echo "MariaDB setup completed successfully."
+
+service mariadb stop
 
 exec "$@"
--- srcs/wordpress/files/setup.sh
+++ srcs/wordpress/files/setup.sh
@@ -1,49 +1,30 @@
 #!/bin/bash
 
+sleep 7
+
 service php7.4-fpm start
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /usr/local/bin/wp
-
-WP_URL="ataboada.42.fr"
-WP_TITLE="Inception"
-WP_ADM_USER="root_user"
-WP_ADM_PASSWORD="password"
-WP_ADM_EMAIL="root_user@42.com"
-WP_USER="user"
-WP_USER_EMAIL="user@42.com"
-WP_USER_ROLE="editor"
-WP_USER_PASSWORD="password"
 
 cd /var/www/wordpress
 
-wp config create \
-		--dbname=db_wordpress \
-		--dbuser=user \
-		--dbpass=user_password \
-		--dbhost=localhost \
-		--allow-root \
-		--extra-php <<PHP
-define( 'WP_HOME', '$WP_URL' );
-define( 'WP_SITEURL', '$WP_URL' );
-PHP
+wp core install \
+		--url=$WP_URL \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADM_USER \
+		--admin_password=$WP_ADM_PASSWORD \
+		--admin_email=$WP_ADM_EMAIL \
+		--skip-email \
+		--allow-root
 
-if ! wp core is-installed --allow-root; then
-	wp core download --allow-root
-	wp core install \
-			--url=$WP_URL \
-			--title=$WP_TITLE \
-			--admin_user=$WP_ADM_USER \
-			--admin_password=$WP_ADM_PASSWORD \
-			--admin_email=$WP_ADM_EMAIL \
-			--skip-email --allow-root
-fi
-
-wp --path=/var/www/wordpress user create $WP_USER $WP_USER_EMAIL \
+if ! wp user list --field=user_login --allow-root | grep -q "^$WP_USER$"; then
+	wp user create $WP_USER $WP_USER_EMAIL \
 		--role=$WP_USER_ROLE \
 		--user_pass=$WP_USER_PASSWORD --allow-root
+fi
 
 service php7.4-fpm stop
 
+exec "$@"
